e5377fc6df2c4b527dedd7e130e81bec160fce95
aalmiray <aalmiray@gmail.com>
2011-01-15 13:55:49 -0600

    first commit; port from Griffon

---
+++ JalarmsGrailsPlugin.groovy
@@ -0,0 +1,95 @@
+/*
+ * grails-jalarms: JAlarms Grails plugin
+ * Copyright 2010 and beyond, Hackergarten
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *  
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *     
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA 
+ */
+
+import com.solab.alarms.AlarmChannel
+import com.solab.alarms.AlarmSender
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+/**
+ * @author Hackergarten
+ */
+class JalarmsGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.5 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    def author = "Your name"
+    def authorEmail = ""
+    def title = "Plugin summary/headline"
+    def description = '''\\
+Brief description of the plugin.
+'''
+
+    def observe = ['controllers', 'services']
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/jalarms"
+
+    def doWithDynamicMethods = { ctx ->
+        processArtifacts()
+    }
+
+    def onChange = { event ->
+        processArtifacts()
+    }
+
+    def onConfigChange = { event ->
+        processArtifacts()
+    }
+
+    private processArtifacts() {
+        def config = ConfigurationHolder.config
+        def application = ApplicationHolder.application
+        def types = config.grails?.jalarms?.injectInto ?: ["Controller", "Service"]
+        types.each { type ->
+            application.getArtefacts(type).each{ klass ->
+                klass.metaClass.sendAlarm = sendAlarm
+            }
+        }
+    }
+    
+    private sendAlarm = {String... args ->
+        ApplicationHolder.application.mainContext.alarmer.sendAlarm(*args)
+    }
+
+    def doWithSpring = {
+        alarmer(AlarmSender) { bean ->
+            bean.scope = 'singleton'
+            bean.autowire = 'byName'
+        }
+    }
+    
+    def doWithApplicationContext = { applicationContext ->
+        Map<String, AlarmChannel> channels = applicationContext.getBeansOfType(AlarmChannel)
+
+        if(channels) {
+            List<AlarmChannel> chnls = []
+            chnls.addAll(channels.values())
+            applicationContext.alarmer.alarmChannels = chnls
+        }
+    }
+}
